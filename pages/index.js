32e218722c9f0d42120695fa06ab620579f34dc5
revell29 <diraapsya0@gmail.com>
2020-07-03 07:32:34 +0700

    first commit

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,282 @@
-import Head from 'next/head'
+import React, { useState, useEffect } from "react";
+import styled from "styled-components";
+import axios from "axios";
 
-export default function Home() {
-  return (
-    <div className="container">
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+function Home(props) {
+    const [visibleOrigin, setVisibleOrigin] = useState(false);
+    const [visibleResi, setVisibleResi] = useState(false);
+    const [visibleDesti, setVisibleDesti] = useState(false);
+    const [kotaAsal, setKotaAsal] = useState("");
+    const [namaKotaAsal, setNamaKotaAsal] = useState("");
+    const [kotaTujuan, setKotaTujuan] = useState("");
+    const [namaKotaTujuan, setNamaKotaTujuan] = useState("");
+    const [berat, setBerat] = useState("");
+    const [dataOngkir, setDataOngkir] = useState([]);
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState({});
+    const [resultManifest, setManifest] = useState({});
+    const [notifBill, setNotifBill] = useState("");
+    const [error, setError] = useState(false);
+    const [kota, setKota] = useState([]);
 
-      <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
+    const selectOriginCity = (e) => {
+        setNamaKotaAsal(event.target.options[event.target.selectedIndex].text);
+        setKotaAsal(e.target.value);
+        setVisibleOrigin(false);
+    };
 
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
+    const selectDestination = (e) => {
+        setNamaKotaTujuan(event.target.options[event.target.selectedIndex].text);
+        setKotaTujuan(e.target.value);
+        setVisibleDesti(false);
+    };
 
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
+    const cekOngkir = (e) => {
+        setLoading(true);
+        axios
+            .post("https://ongkir-api.vercel.app/cek_harga", {
+                kota_asal: kotaAsal,
+                type_asal: "city",
+                kota_tujuan: kotaTujuan,
+                type_tujuan: "city",
+                berat: berat,
+            })
+            .then((res) => {
+                setLoading(false);
+                setDataOngkir(res.data.data.results);
+            });
+    };
 
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+    const cekResi = (e) => {
+        e.preventDefault();
+        setError(false);
+        axios
+            .post("https://ongkir-api.vercel.app/check_resi", {
+                resi: data.resi,
+                kurir: data.kurir,
+            })
+            .then((res) => {
+                if (res.data.data.status.code === 200) {
+                    setManifest(res.data.data.result.manifest);
+                } else {
+                    setError(true);
+                    setNotifBill("No Resi tidak terdaftar.");
+                }
+            });
+    };
 
-          <a
-            href="https://github.com/vercel/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+    useEffect(() => {
+        axios.get("https://ongkir-api.vercel.app/kota").then((res) => setKota(res.data.data));
+    }, []);
 
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
-      </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
-  )
+    return (
+        <>
+            {visibleResi && (
+                <div className="h-screen w-full absolute">
+                    <div
+                        onClick={(e) =>
+                            setVisibleResi((prevState) => {
+                                !prevState;
+                                setManifest({});
+                                setError(false);
+                                setData({});
+                            })
+                        }
+                        className="bg-black absolute h-screen w-full opacity-50 z-20"
+                    ></div>
+                    <ModalResi className="bg-white p-5 left-0 right-0 rounded-lg absolute z-50">
+                        <h1 className="text-3xl text-grey-500 font-semibold">Pilih Kota Asal</h1>
+                        <select className="w-full p-5 mt-3 bg-gray-300 rounded-lg focus:outline-none" onChange={(e) => setData({ ...data, kurir: e.target.value })}>
+                            <option>Pilih Kurir</option>
+                            <option value="jne">JNE</option>
+                            <option value="jnt">J&T</option>
+                            <option value="sicepat">Sicepat</option>
+                            <option value="pos">POS Indonesia</option>
+                            <option value="tiki">Tiki</option>
+                            <option value="ninja">Ninja Express</option>
+                            <option value="wahana">Wahana</option>
+                        </select>
+                        <input
+                            type="text"
+                            className="w-full p-5 mt-3 bg-gray-300 rounded-lg focus:outline-none"
+                            placeholder="Masukan no resi anda"
+                            required
+                            onChange={(e) => setData({ ...data, resi: e.target.value })}
+                        />
+                        <button className="p-5 text-white bg-green-500 rounded w-full mt-3" onClick={(e) => cekResi(e)}>
+                            Cek Resi
+                        </button>
+                        {error && <div className="bg-orange-500 p-4 mt-3 text-white">{notifBill}</div>}
+                        {resultManifest.length > 0 && (
+                            <div className="mt-5 overflow-y-auto" style={{ height: "30rem" }}>
+                                <h4>Details</h4>
+                                {resultManifest.map((item) => (
+                                    <div className="flex-col mb-4 mt-5">
+                                        <h4>
+                                            {item.city_name} ({item.manifest_description})
+                                        </h4>
+                                        <p className="text-sm text-gray-600">
+                                            {item.manifest_date} {item.manifest_time}
+                                        </p>
+                                    </div>
+                                ))}
+                            </div>
+                        )}
+                    </ModalResi>
+                </div>
+            )}
+            {visibleOrigin && (
+                <div className="h-full w-full">
+                    <div onClick={(e) => setVisibleOrigin(false)} className="bg-black absolute h-full w-full opacity-50 z-20"></div>
+                    <Modal className="bg-white bottom-0 p-5 left-0 right-0 rounded-t-lg absolute z-50">
+                        <h1 className="text-3xl text-grey-500 font-semibold">Pilih Kota Asal</h1>
+                        <select className="w-full p-5 mt-3 bg-gray-300 rounded-lg focus:outline-none" onChange={(e) => selectOriginCity(e)}>
+                            {kota.results.map((item) => (
+                                <option value={item.city_id}>{item.city_name}</option>
+                            ))}
+                        </select>
+                    </Modal>
+                </div>
+            )}
+            {visibleDesti && (
+                <div className="h-full w-full">
+                    <div onClick={(e) => setVisibleDesti(false)} className="bg-black absolute h-full w-full opacity-50 z-20"></div>
+                    <Modal className="bg-white bottom-0 p-5 left-0 right-0 rounded-t-lg absolute z-50">
+                        <h1 className="text-3xl text-grey-500 font-semibold">Pilih Kota Tujuan</h1>
+                        <select className="w-full p-5 mt-3 bg-gray-300 rounded-lg focus:outline-none" onChange={(e) => selectDestination(e)}>
+                            {kota.results.map((item) => (
+                                <option value={item.city_id}>{item.city_name}</option>
+                            ))}
+                        </select>
+                    </Modal>
+                </div>
+            )}
+            <Section>
+                <Section2>
+                    <header className="flex items-center justify-between p-5">
+                        <h1 className="text-2xl uppercase text-blue-500">CEKKURIR</h1>
+                        <button onClick={(e) => setVisibleResi(true)} className="p-2 border-2 border-blue-500 rounded text-blue-500 hover:bg-blue-500 hover:text-white focus:outline-none">
+                            Cek Resi
+                        </button>
+                    </header>
+                    <div className="bg-blue-400 p-5 border-l-4 border-blue-700 mb-5">
+                        <p className="text-white text-1xl">Website untuk cek resi dan cek ongkir.</p>
+                    </div>
+                    <div className="mb-2 mx-5">
+                        <div onClick={(e) => setVisibleOrigin((prevState) => !prevState)} className="bg-white p-5 rounded-lg flex items-center justify-between">
+                            <h4>Kota Asal</h4>
+                            <h4>{namaKotaAsal}</h4>
+                            <button className="rounded bg-gray-300 p-3 px-4 inline-flex items-center relative hover:bg-gray-400 focus:outline-none">
+                                Pilih
+                                <svg viewBox="0 0 24 24" focusable="false" role="presentation" className="ml-2 h-6 w-6 inline-block">
+                                    <path fill="currentColor" d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"></path>
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                    <div className="mb-2 mx-5">
+                        <div onClick={(e) => setVisibleDesti((prevState) => !prevState)} className="bg-white p-5 rounded-lg flex items-center justify-between">
+                            <h4>Kota Tujuan</h4>
+                            <h4>{namaKotaTujuan}</h4>
+                            <button className="rounded bg-gray-300 p-3 px-4 inline-flex items-center relative hover:bg-gray-400 focus:outline-none">
+                                Pilih
+                                <svg viewBox="0 0 24 24" focusable="false" role="presentation" className="ml-2 h-6 w-6 inline-block">
+                                    <path fill="currentColor" d="M16.59 8.59L12 13.17 7.41 8.59 6 10l6 6 6-6z"></path>
+                                </svg>
+                            </button>
+                        </div>
+                    </div>
+                    <div className="mb-5 mx-5">
+                        <div className="bg-white p-5 rounded-lg flex items-center justify-between">
+                            <input onChange={(e) => setBerat(e.target.value)} type="text" placeholder="Berat" className="p-3 bg-gray-200 rounded-lg focus:outline-none" />
+                            <h4 className="mr-5">(Gram)</h4>
+                        </div>
+                    </div>
+                    <div className="mb-5 mx-5">
+                        <button onClick={(e) => cekOngkir(e)} className="bg-green-500 rounded-lg p-5 w-full text-white shadow hover:bg-green-600" disabled={loading}>
+                            {loading ? "Please wait" : "Cek Ongkir"}
+                        </button>
+                    </div>
+                    {dataOngkir.map((item) => (
+                        <div className="">
+                            {item.costs.map((data) => (
+                                <div className="mb-2 mx-5 rounded-lg bg-white p-5 flex items-center justify-between">
+                                    <h4 className="uppercase">{item.code}</h4>
+                                    <div className="flex-col">
+                                        <h4>{data.service}</h4>
+                                    </div>
+                                    {data.cost.map((cost) => (
+                                        <div className="flex-col text-right">
+                                            <h4>{cost.etd} Hari</h4>
+                                            <p>Rp {cost.value}</p>
+                                        </div>
+                                    ))}
+                                </div>
+                            ))}
+                        </div>
+                    ))}
+                </Section2>
+            </Section>
+            <Footer>
+                <p className="ml-4">
+                    Build with<span className="mr-2">❤</span>Apsyadira
+                </p>
+            </Footer>
+        </>
+    );
 }
+
+const Section = styled.div`
+    margin-left: auto;
+    margin-right: auto;
+    width: 100%;
+    max-width: 32rem;
+    background-color: #edf2f7;
+    -webkit-box-flex: 1;
+    -webkit-flex-grow: 1;
+    -ms-flex-positive: 1;
+    flex-grow: 1;
+    padding-bottom: 4rem;
+    overflow-y: auto;
+    height: calc(var(--vh, 1vh) * 90);
+`;
+
+const Footer = styled.div`
+    justify-content: space-between;
+    position: absolute
+    padding-left: 2rem;
+    padding-right: 2rem;
+    padding-top: 2rem;
+    padding-bottom: 2rem;
+    width: 100%;
+    max-width: 32rem;
+    margin-left: auto;
+    margin-right: auto;
+`;
+
+const Section2 = styled.div`
+    display: -webkit-box;
+    display: -webkit-flex;
+    display: -ms-flexbox;
+    display: flex;
+    -webkit-flex-direction: column;
+    -ms-flex-direction: column;
+    flex-direction: column;
+`;
+
+const Modal = styled.div`
+    margin: 0px auto;
+    max-width: 32rem;
+    width: 100%;
+    height: 22rem;
+    border-top-left-radius: 30px;
+    border-top-right-radius: 30px;
+`;
+
+const ModalResi = styled.div`
+    margin: 5em auto;
+    max-width: 32rem;
+    width: 100%;
+`;
+
+export default Home;
